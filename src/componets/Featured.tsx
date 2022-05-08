eb94163984801d9b61179a0e63bd173bbc83c913
Matt Graham <iam@mattgrah.am>
2022-05-08 17:21:07 +1000

    complete home page

--- src/componets/Featured.tsx
+++ src/componets/Featured.tsx
@@ -4,50 +4,22 @@ import dummy from "../img/dummy_200x200.png";
 export const Featured = () => {
   return (
     <>
-      <hr className="mt-20 border-dashed border-neutral-800 dark:border-neutral-200" />
+      <div className="hr mt-20"></div>
       <Link to="/projects">
         <h2 className="mt-16 text-4xl font-bold underline decoration-blue-600">
           Featured Projects
         </h2>
       </Link>
       <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-800 dark:text-neutral-300">
-        A collection of some side projects that I have completed recently.
+        A collection of projects that I have completed.
       </p>
-      <div className="my-6 flex gap-4 rounded border border-neutral-700 p-4">
-        <img src={dummy} alt="" />
+      <div className="my-6 flex gap-4 rounded border border-neutral-400 bg-neutral-300 p-4 dark:border-neutral-700 dark:bg-neutral-800">
+        <img className="shadow-md" src={dummy} alt="" />
         <div>
           <h3 className="text-3xl font-bold leading-tight text-neutral-800 dark:text-neutral-200">
             Featured Project Title
           </h3>
-          <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-800 dark:text-neutral-300">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
-            vero ex id quasi ea repudiandae ut repellendus ipsum cum excepturi
-            beatae, possimus necessitatibus rem aspernatur nemo sint? Deserunt,
-            sapiente sequi.
-          </p>
-        </div>
-      </div>
-      <div className="my-6 flex gap-4 rounded border border-neutral-700 p-4">
-        <img src={dummy} alt="" />
-        <div>
-          <h3 className="text-3xl font-bold leading-tight text-neutral-800 dark:text-neutral-200">
-            Featured Project Title
-          </h3>
-          <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-800 dark:text-neutral-300">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
-            vero ex id quasi ea repudiandae ut repellendus ipsum cum excepturi
-            beatae, possimus necessitatibus rem aspernatur nemo sint? Deserunt,
-            sapiente sequi.
-          </p>
-        </div>
-      </div>
-      <div className="my-6 flex gap-4 rounded border border-neutral-700 p-4">
-        <img src={dummy} alt="" />
-        <div>
-          <h3 className="text-3xl font-bold leading-tight text-neutral-800 dark:text-neutral-200">
-            Featured Project Title
-          </h3>
-          <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-800 dark:text-neutral-300">
+          <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-700 dark:text-neutral-300">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
             vero ex id quasi ea repudiandae ut repellendus ipsum cum excepturi
             beatae, possimus necessitatibus rem aspernatur nemo sint? Deserunt,
