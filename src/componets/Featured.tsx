dd56e6608904d23ab9d350fb09d3ea36e8d08b3e
Matt Graham <iam@mattgrah.am>
2022-05-10 21:09:59 +1000

    add project content

--- src/componets/Featured.tsx
+++ src/componets/Featured.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FeaturedItems } from "./ProjectItems";
 
 export const Featured = () => {
   return (
@@ -12,24 +13,7 @@ export const Featured = () => {
       <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-800 dark:text-neutral-300">
         A collection of projects that I have completed.
       </p>
-      <div className="my-6 flex gap-4 rounded border border-neutral-400 bg-neutral-300 p-4 dark:border-neutral-700 dark:bg-neutral-800">
-        <img
-          className="rounded border border-neutral-400 dark:border-neutral-700"
-          src="https://dummyimage.com/200x200/cccccc/ffffff"
-          alt=""
-        />
-        <div>
-          <h3 className="text-3xl font-bold leading-tight text-neutral-800 dark:text-neutral-200">
-            Featured Project Title
-          </h3>
-          <p className="mt-2 text-lg leading-normal tracking-wider text-neutral-700 dark:text-neutral-300">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
-            vero ex id quasi ea repudiandae ut repellendus ipsum cum excepturi
-            beatae, possimus necessitatibus rem aspernatur nemo sint? Deserunt,
-            sapiente sequi.
-          </p>
-        </div>
-      </div>
+      <FeaturedItems />
     </>
   );
 };
--- src/componets/Projects.tsx
+++ src/componets/Projects.tsx
@@ -1,27 +1,16 @@
-import { ProjectItem } from "./ProjectItem";
+import { ProjectItem, ProjectTiles } from "./ProjectItems";
 
 export const Projects = () => {
   return (
     <div className="container">
       <h2 className="text-5xl font-bold">Projects</h2>
       <p className="about-p">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio optio
-        consequatur non odit blanditiis deserunt, exercitationem dolorum libero
-        asperiores sit modi voluptatibus explicabo. Doloribus amet velit quam
-        sed, dicta vero!
+        Starting at a simple Tic Tac Toe game, to a Job Application Tracker, to
+        a complex music guessing game using Spotify's API. This is a collection
+        of projects I have completed whilst on my journey as a Software
+        developer.
       </p>
-      <div className="flex gap-4">
-        <div className="my-6 flex w-52 flex-col gap-4 rounded border border-neutral-400 bg-neutral-300 p-2 dark:border-neutral-700 dark:bg-neutral-800">
-          <img
-            className="rounded border border-neutral-400 dark:border-neutral-700"
-            src="https://dummyimage.com/200x200/cccccc/ffffff"
-            alt=""
-          />
-          <p className="text-normal text-center font-bold text-neutral-800 dark:text-neutral-200">
-            Project Title
-          </p>
-        </div>
-      </div>
+      <ProjectTiles />
       <div className="hr mt-8"></div>
       <ProjectItem />
     </div>
