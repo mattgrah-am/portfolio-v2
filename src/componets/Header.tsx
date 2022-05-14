50eec74974672deaa234923865b3a063e4aecba8
Matt Graham <iam@mattgrah.am>
2022-05-14 14:13:41 +1000

    change of nav

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -4,13 +4,15 @@ export const Header = () => {
   return (
     <header>
       <div className="fixed top-0 h-1 w-full bg-gradient-to-r from-red-500 via-blue-600 to-purple-700"></div>
-      <div className="fixed top-1 mb-10 flex w-full items-center justify-between border-b border-neutral-300 bg-neutral-100/80 py-3 px-10 backdrop-blur-lg dark:border-neutral-700 dark:bg-neutral-900/80">
-        <a href="/">
-          <h1 className="m-2 justify-self-start p-0 text-4xl font-bold tracking-tighter text-neutral-900 dark:text-neutral-100">
-            mg.
-          </h1>
-        </a>
-        <Nav />
+      <div className="fixed top-1 mb-10 w-full border-b border-neutral-300 bg-neutral-100/80 py-3 backdrop-blur-lg dark:border-neutral-700 dark:bg-neutral-900/80 ">
+        <div className="mx-auto flex w-full max-w-4xl items-center justify-between px-2 sm:px-0 ">
+          <a href="/">
+            <h1 className="my-2 p-0 text-4xl font-bold tracking-tighter text-neutral-900 dark:text-neutral-100">
+              mg.
+            </h1>
+          </a>
+          <Nav />
+        </div>
       </div>
     </header>
   );
